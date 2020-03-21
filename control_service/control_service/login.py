bbc6e6937393e9568cc9e7e32dc1bb026de49b06
vilaureu <vilaureu@users.noreply.github.com>
2020-03-21 23:21:51 +0100

    hashed and salted passwords

--- control_service/control_service/login.py
+++ control_service/control_service/login.py
@@ -3,6 +3,7 @@ from functools import wraps
 from control_service import app, db
 from control_service.models import UserData
 from control_service.schemas import LOGINSCHEMA, get_validated_json
+from werkzeug.security import check_password_hash
 
 
 @app.route('/Login', methods=['POST'])
@@ -12,12 +13,15 @@ def login():
     reqUsername = data['Username']
     reqPassword = data['Password']
 
-    userEntity = db.session.query(UserData).filter_by(user_name = reqUsername, password = reqPassword).first()
+    userEntity = db.session.query(UserData).filter_by(user_name = reqUsername).first()
 
     response = {}
-    if(userEntity is not None):
-        response['Token'] = userEntity['token']
+    if userEntity is not None:
+        response['Token'] = userEntity.token
     else:
+        abort(403)
+
+    if not check_password_hash(userEntity.password, reqPassword):
         abort(403)
 
     return jsonify(response)
