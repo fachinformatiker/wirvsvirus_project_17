635311f7c91dbe5f327071c5b2dcd81d9af4aef7
scientes <34819304+scientes@users.noreply.github.com>
2020-03-21 16:00:20 +0100

    prototype endpoints

--- control_service/control_service/views.py
+++ control_service/control_service/views.py
@@ -1,4 +1,5 @@
 from functools import wraps
+from flask import jsonify
 from flask import request, abort
 from control_service import app
 
@@ -6,16 +7,25 @@ from control_service import app
 def hello_world():
     return 'Hello World!'
 
-@app.route('/market/<int:user_id>/get')
+@app.route('/market/getbyid/<int:id>')
 def get_market(id):
-    return id
+    print(id)
+    market = {"marketid": 42, "Name": "TollerNetto", "Company": "aldi", "GPSlocation": {"lat": 42.1231, "long": 100.1},
+              "Adresse": "Straße", "status": 1, "timestamp": "01-01-1970"}
+
+    return jsonify(market)
 
 @app.route('/market/get')
 def get_market_body():
-    print(request)
-    if(request.is_json == True and request.method == 'GET'):
-        return request.json
-    return {}
-@app.route('/marketlist/')
-def get_marketlist(id):
-    return 'Hello World!'
+    #help needed
+    if(request.method == 'GET'):
+        market={"marketid":42,"Name":"TollerNetto","Company":"aldi","GPSlocation":{"lat":42.1231,"long":100.1},"Adresse":"Straße","status":1,"timestamp":"01-01-1970"}
+        return jsonify(market)
+    abort(400)
+
+@app.route('/marketlist/<float:lat>/<float:long>/')
+def get_marketlist(lat,long):
+    if (request.method == 'GET'):
+        marketlist={"Markets":[{"marketid":42,"Name":"TollerNetto","Company":"aldi","GPSlocation":{"lat":42.1231,"long":100.1},"Adresse":"Straße","status":1,"timestamp":"01-01-1970"}]}
+        return jsonify(marketlist)
+    abort(400)
