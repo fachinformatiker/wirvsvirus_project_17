08a44f91e11b74c75718f5f3d3eafce4d874f7d5
redisun <35756939+redisun@users.noreply.github.com>
2020-03-22 10:32:54 +0100

    add inputservice market_status_change

--- input_service/input_service/views.py
+++ input_service/input_service/views.py
@@ -1,7 +1,29 @@
-from functools import wraps
+import requests
 from flask import request, abort
-import jwt
-from input_service import app, auth, db
-from input_service.models import UserData, Stammdaten
+from input_service import app
 
+@app.route('/market/status', methods=['POST'])
+def handle_market_status_change():
+  content = request.json
+  market_id = content['MarketID']
+  token = content['Token']
+  status = content['Status']
 
+  try:
+    auth_request = requests.post('http://controlservice/GetUserProfil', data = {'Token':token}, verify=False)
+  except requests.exceptions.ConnectionError:
+    abort(400)
+
+  auth_response = auth_request.json()
+
+  if auth_response['MarketId'] == market_id:
+    try:
+      status_update_request = requests.post('http://controlservice/market/status', data = {'MarketId': market_id, 'Status': status}, verify=False)
+    except requests.exceptions.ConnectionError:
+      abort(400)
+  
+  status_update_response = status_update_request.json()
+
+  return {
+    "Success": status_update_response['Success']
+  }
